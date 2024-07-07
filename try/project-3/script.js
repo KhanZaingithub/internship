1bd344e3821c4e692517170b45381ee8c637a241
Zainul khan <109201380+KhanZaingithub@users.noreply.github.com>
2024-07-07 11:49:00 +0530

    Add files via upload

---
+++ try/project-3/script.js
@@ -0,0 +1,21 @@
+// const link = document.querySelectorAll(".nav-link")
+// // link[2].addEventListener("click",()=>{
+// //     console.log("hello");
+// // })
+// link.(function (i) {
+//     i.addEventListener('click', function() {
+//         for(let j = 0;j<6;j++){
+//             if(i.classList.contains("active")){
+//                 i.classList.remove("active")
+//                 console.log("hello");
+//             }
+//         }
+
+//         if(i.classList.contains("active")){
+//             return;
+//         }
+//         else{
+//             i.classList.add("active")
+//         }
+//     });
+//   });
