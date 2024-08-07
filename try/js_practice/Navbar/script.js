2a74ba4d37d47e51d6cd1dc24dfc9e7508f8b519
Zainul khan <109201380+KhanZaingithub@users.noreply.github.com>
2024-08-07 18:24:21 +0530

    Delete try/js_practice directory

--- try/js_practice/Navbar/script.js
+++
@@ -1,5 +0,0 @@
-const nav = document.querySelector(".navbar-toggler");
-const item = document.querySelector("#navbarSupportedContent-1");
-nav.addEventListener("click",function (){
-    item.classList.toggle("show");
-})
